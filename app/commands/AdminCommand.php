a8b4dab8d682752e334559c0b987f02f1b490f7b
andrew <ygyman@gmail.com>
2013-02-08 15:29:47 +0200

    admin command

--- app/commands/AdminCommand.php
+++ app/commands/AdminCommand.php
@@ -41,8 +41,10 @@ class AdminCommand extends Command {
         $confirmed = false;
         $password = null;
 
-        $name = $this->ask('Enter username:');
+        //Get email from console
+        $email = $this->ask('Enter email:');
 
+        //Get password from console
         while (!$confirmed) {
             if (!$password) {
                 $password = $this->ask('Enter password:');
@@ -61,8 +63,50 @@ class AdminCommand extends Command {
             }
         }
 
-        //TODO: store superadin data in db
-        $this->info('Storing ' . $name . ':' . $password . ' in db');
+        $adminPermissions = [
+            'users' => [
+                'read'      => 1,
+                'write'     => 1,
+                'delete'    => 1,
+            ],
+            'roles' => [
+                'read'      => 1,
+                'write'     => 1,
+                'delete'    => 1,
+            ],
+            'sites' => [
+                'read'      => 1,
+                'write'     => 1,
+                'delete'    => 1,
+            ],
+        ];
+
+        $roleData = [
+            'name' => 'Admin Role',
+            'permissions' => $adminPermissions
+        ];
+
+        //Store role in db
+        $response = $this->call('POST', '/roles', array(), array(), array(), json_encode($roleData));
+        $response = json_decode($response->getContent(), true);
+
+        $roleResponse = $response['roles'][0];
+
+        $userData = [
+            'first_name'    => 'Admin',
+            'last_name'     => '',
+            'email'         => $email,
+            'password'      => $password,
+            'roles'         => [$roleResponse['_id']],
+            'permissions'   => $adminPermissions
+        ];
+
+        //Store user in db
+        $response = $this->call('POST', '/users', array(), array(), array(), json_encode($userData));
+        /*$response = json_decode($response->getContent(), true);
+        $userResponse = $response['users'][0];*/
+
+        $this->info('Storing in db');
 	}
 
 	/**
