467fb660a1f713884c7c32b22e452a227fa65678
jsamos <jsamos@gmail.com>
2013-03-05 18:32:32 -0500

    re-add default db

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -3,6 +3,13 @@
 return array(
 
 	'mongodb' => array(
+
+        'default' => array(
+            'host' => 'prod.eventbidz.com',
+            'port' => 27017,
+            'database' => 'slender',
+        ),
+
 		'eb' => array(
 			'host' => 'prod.eventbidz.com',
 			'port' => 27017,
--- app/config/staging/database.php
+++ app/config/staging/database.php
@@ -2,6 +2,12 @@
 
 return array(
 
+    'default' => array(
+        'host' => 'stage.eventbidz.com',
+        'port' => 27017,
+        'database' => 'slender',
+    ),
+
 	'mongodb' => array(
 		'eb' => array(
 			'host' => 'stage.eventbidz.com',
