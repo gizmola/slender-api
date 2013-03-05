45e12119aedf186e1101b1e57f77a78880fc6556
jsamos <jsamos@gmail.com>
2013-03-04 19:53:10 -0500

    add keywords and service categories

---
+++ app/Slender/API/Model/Site/Eb/Keywords.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Slender\API\Model\Site\Eb;
+
+use \Slender\API\Model\BaseModel as BaseModel;
+
+class Keywords extends BaseModel
+{
+    
+    protected $schema = [
+        'name'    => ['required'],
+    ];
+
+}
