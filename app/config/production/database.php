caf38193cca73bb9462ba2c1900f5b2e0a0c19f9
jsamos <jsamos@gmail.com>
2013-03-05 16:34:02 -0500

    config for dbs

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -3,25 +3,10 @@
 return array(
 
 	'mongodb' => array(
-		// 'default' => array(
-		// 	'host' => 'slender.example.com',
-		// 	'port' => 27017,
-		// 	'database' => 'slender',
-		// ),
-		'default' => array(
-			'host' => '127.0.0.1',
+		'eb' => array(
+			'host' => 'prod.eventbidz.com',
 			'port' => 27017,
-			'database' => 'slender',
-		),
-		'ai' => array(
-			'host' => 'slender.example.com',
-			'port' => 27017,
-			'database' => 'slender_ai',
-		),
-		'demo' => array(
-			'host' => '127.0.0.1',
-			'port' => 27017,
-			'database' => 'slender_demo',
+			'database' => 'slender_eb',
 		),
 	),		
 );
--- app/config/staging/database.php
+++ app/config/staging/database.php
@@ -3,21 +3,6 @@
 return array(
 
 	'mongodb' => array(
-		'default' => array(
-			'host' => 'stage.eventbidz.com',
-			'port' => 27017,
-			'database' => 'slender',
-		),
-		'ai' => array(
-			'host' => 'stage.eventbidz.com',
-			'port' => 27017,
-			'database' => 'slender_ai',
-		),
-		'demo' => array(
-			'host' => 'stage.eventbidz.com',
-			'port' => 27017,
-			'database' => 'slender_demo',
-		),
 		'eb' => array(
 			'host' => 'stage.eventbidz.com',
 			'port' => 27017,
