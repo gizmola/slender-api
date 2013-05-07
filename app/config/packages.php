7356907f528b8b71380b929fef6d631f19bda823
jsamos <jsamos@gmail.com>
2013-05-07 14:15:53 -0400

    allow for packages to be listed

---
+++ app/config/packages.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Installed Packages
+    |--------------------------------------------------------------------------
+    |
+    | Currently laravel does not appear to have a way of determining the 
+    | packages currently installed packages. This config allows them to
+    | be accessed at runtime
+    |
+    */
+
+
+)
