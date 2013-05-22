ba850baa109bca57706dcc252aeddd9d7fa964fa
jsamos <jsamos@gmail.com>
2013-05-22 17:54:59 -0400

    remove mongo.ini template

--- recipes/cookbooks/php/recipes/pear_mongo.rb
+++ recipes/cookbooks/php/recipes/pear_mongo.rb
@@ -2,10 +2,3 @@
 php_pear "mongo" do
   action :install
 end
-
-template "#{node['php']['ext_conf_dir']}/20-mongo.ini" do
-  source "mongo.ini.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-end
