a96abdf134a8634eaee97a931728d90456b46131
jsamos <jsamos@gmail.com>
2013-06-06 10:41:40 -0400

    remove old recipes

--- vagrant/cookbooks/slender-api/recipes/composer.rb
+++
@@ -1,8 +0,0 @@
-bash "composer" do
-    user "root"
-    not_if "which composer"
-    code <<-EOH
-        curl -sS https://getcomposer.org/installer | php
-        mv composer.phar /usr/local/bin/composer
-    EOH
-end
--- vagrant/cookbooks/slender-api/recipes/scm.rb
+++
@@ -1,9 +0,0 @@
-package "git" do
-  not_if "which git"
-  action :install
-end
-
-package "subversion" do
-  not_if "which svn"
-  action :install
-end
